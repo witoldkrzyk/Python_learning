4d5f423b1234bb838a397d62fe95c689878b0039
Witold Krzv <witoldkrzyk@gmail.com>
2020-03-21 19:51:10 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,65 @@
+# Przywitanie po imieniu
+
+#name = 'Witold'  # this is my name
+
+#print(f'Hello world \n\t{name}')
+
+
+#value1 = 1
+#value2 = 10
+#print(value1 < value2)
+
+#min([1,2,3,4,5])
+
+# komentarze !!!
+
+bit = 1
+kilobit = 1024 * bit
+megabit = 1024 * kilobit
+
+byte = 8 * bit
+kilobyte = 1024 * byte
+megabyte = 1024 * kilobyte
+
+print(f'1 MB in bit = {1*megabyte / bit}')
+print(f'1 MB in Mb = {1*megabyte / megabit} Mb')
+
+# time
+""""
+second = 1
+minute = 60 * second
+hour = 60 * minute
+day = 24 * hour
+
+print(f' 5 min in = {5 * minute} sec')
+print(f'5 min in seconds = {5 * minute / second}')
+print(f'1 hour in seconds = {1 * hour / second}')
+print(f'One day of work (8h) in seconds = {8 * hour / second}')
+print(f'One month of work (22days for 8hours) in seconds = {22 * 8 * hour / second }')
+print(f'One week of work (40h) in minutes = {40 * hour / minute}')
+"""
+'''
+bit = 1
+kilobit = 1024 * bit
+megabit = 1024 * kilobit
+
+byte = 8 * bit
+kilobyte = 1024 * byte
+megabyte = 1024 * kilobyte
+
+file_size = 100 * megabyte
+speed = 100 * megabit
+time = file_size / speed
+
+print(f'Download time: {time} sec')
+'''
+
+'''
+value = -21.37
+print(value)
+
+million = 1e6
+print(million)
+'''
+
+#
