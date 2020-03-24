2d4b487e769b831a584acec70c7b493beb6379fc
Witold Krzv <witoldkrzyk@gmail.com>
2020-03-24 20:01:55 +0100

    add validation

--- calculator.py
+++ calculator.py
@@ -1,33 +1,49 @@
-#calculator dwoch liczb
+#calculator
+
+def inputNumber(message):
+    while True:
+        try:
+            userInput = int(input(message))
+        except ValueError:
+            print('To nie jest liczba! Wpisz liczbę')
+            continue
+        else:
+            return userInput
+            break
+
 
 print("Witaj w kalkulatorze ! \U0001F642")
 
-myslniki = '-' * 30
-print(myslniki)
+dash = '-' * 30
+print(dash)
 
 #dodawanie
-def dodawanie(x, y):
+def addition(x, y):
     return x + y
 
 #odejmowanie 
-def odejmowanie(x, y):
+def subtraction(x, y):
     return x - y
 
 #mnozenie 
-def mnozenie(x, y):
+def multiplication(x, y):
     return x * y
 
 #dzielenie 
-def dzielenie(x, y):
+def division(x, y):
     return x / y 
 
 #potegowanie 
-def potegowanie(x, y):
+def exponentiation(x, y):
     return x ** y
 
 #modulo
 def modulo(x, y):
     return x % y
+
+#dzielenie_zaokraglone  
+def trueDivision(x, y):
+    return x // y
 
 print('Wybierz operacje: ')
 print('1. Dodawanie')
@@ -36,28 +52,31 @@ print('3. Mnozenie')
 print('4. Dzielenie')
 print('5. Potegowanie')
 print('6 Modulo pierwszej z drugiej')
+print('7. Dzielenie zaokrąglone')
+choice = inputNumber('Wybierz co chcesz zrobic: 1 / 2 / 3 / 4 / 5 / 6 / 7 :  ')
 
-wybor = int(input('Wybierz co chcesz zrobic: 1 / 2 / 3 / 4 / 5 / 6 :  '))
-
-x = float(input('Wpisz pierwsza liczbe: '))
-y = float(input('Wpisz druga liczbe: '))
+x = inputNumber('Wpisz pierwszą liczbę: ')
+y = inputNumber('Wpisz drugą liczbę: ')
 
 
 
-if wybor == 1: 
-    print(f'{x} + {y} = {dodawanie(x, y)}')
+if choice == 1: 
+    print(f'{x} + {y} = {addition(x, y)}')
 
-elif wybor == 2:
-    print(f'{x} - {y} = {odejmowanie(x, y)}')
+elif choice == 2:
+    print(f'{x} - {y} = {subtraction(x, y)}')
 
-elif wybor == 3:
-    print(f'{x} * {y} = {mnozenie(x, y)}')
+elif choice == 3:
+    print(f'{x} * {y} = {multiplication(x, y)}')
 
-elif wybor == 4:
-    print(f'{x} / {y} = {dzielenie(x, y)}')
+elif choice == 4:
+    print(f'{x} / {y} = {division(x, y)}')
 
-elif wybor == 5:
-    print(f'{x} ^ {y} = {potegowanie(x, y)}')
+elif choice == 5:
+    print(f'{x} ^ {y} = {exponentiation(x, y)}')
 
-elif wybor == 6:
+elif choice == 6:
     print(f'{x} % {y} = {modulo(x, y)}')
+
+elif choice == 7:
+    print(f'({x} // {y} = {trueDivision(x, y)}')
