3a3329e5ba0da6081209c186f06826852b710a84
Witold Krzv <witoldkrzyk@gmail.com>
2020-03-30 03:36:32 +0200

    add testy-nauka

--- testy-do-nauki.py
+++ testy-do-nauki.py
@@ -1,3 +1,6 @@
+
+#   __dict__)   #
+'''
 class Iris:
     pass
 
@@ -9,3 +12,26 @@ flower.petal_width = 0.2
 flower.species = 'setosa'
 
 print(flower.__dict__)
+'''
+
+
+
+# __init__  #
+#walidacja na float i int
+
+class Kelvin:
+    def __init__(self, value):
+        if not isinstance(value, (float, int)):
+            raise TypeError('Temperature must be int or float')
+
+        if value < 0.0:
+            raise ValueError('Temperature must be greater than 0')
+
+        self.value = value
+    
+ice = Kelvin(273.15)
+print(ice.value)
+#273.15
+
+not_existing = Kelvin(-300)
+#ValueError
